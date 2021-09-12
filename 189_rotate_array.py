2a1b4577871f814f6dd93cf0b81b8cea9339e7d3
Richard Khonan <richardkhonan0797@gmail.com>
2021-09-12 22:21:55 +0700

    number 189

--- 189_rotate_array.py
+++ 189_rotate_array.py
@@ -33,4 +33,4 @@ def rotate(nums: List[int], k: int) -> None:
     print(nums)
             
 rotate(nums=[1, 2, 3, 4, 5, 6, 7], k=3) # [5,6,7,1,2,3,4]
-rotate(nums=[1, 2], k=5)
+rotate(nums=[1, 2], k=5) # [2, 1]
