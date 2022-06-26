c1654f6c644fc7ceba64882db6e3a136d985af70
Richard Khonan <55327505+richardkhonan0797@users.noreply.github.com>
2022-06-26 17:37:01 +0700

    Update and rename 125_valid_palindrome.py to 125_valid_palindrome.ts

---
+++ 125_valid_palindrome.ts
@@ -0,0 +1,38 @@
+"""
+Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+"""
+
+function isPalindrome(s: string): boolean {
+    if (s.length === 1) return true;
+    
+    let charsOnly = "";
+    
+    for (let i = 0; i < s.length; i++) {
+        let ascii = s.charCodeAt(i);
+        if (ascii >= 65 && ascii <= 90 || ascii >= 97 && ascii <= 122) {
+            charsOnly += s[i].toLowerCase();
+        }
+    }
+    
+    let i = 0;
+    let j = charsOnly.length - 1;
+    
+    while (i < j) {
+        if (s[i] !== s[j]) return false;
+        i++;
+        j--;
+    }
+    
+    return true;
+};
